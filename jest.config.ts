060e4a5e9b8333f89fe99a13c9a4753155bdb7c5
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-04-10 23:54:30 -0300

    Initial Commit

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest',
+
+  testEnvironment: 'node',
+
+  testMatch: ['<rootDir>/src/**/*.spec.ts', '<rootDir>/test/integration/*.spec.ts'],
+
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+
+  transformIgnorePatterns: ['/node_modules/(?!uuid)'],
+
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
