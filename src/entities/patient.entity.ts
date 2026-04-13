cf5b508675eae5a86254b2ad3252ea4f0f9df230
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-04-13 19:20:42 -0300

    Add `PatientEntity` for patient database integration

---
+++ src/entities/patient.entity.ts
@@ -0,0 +1,30 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, Index, PrimaryColumn, Unique, UpdateDateColumn } from 'typeorm';
+import type { AuthUser } from 'src/clients/auth/auth.interface';
+
+@Unique('UQ_patient_person_id', ['person_id'])
+@Unique('UQ_patient_medical_record_number', ['medical_record_number'])
+@Entity('patient')
+export class PatientEntity extends BaseEntity {
+  @PrimaryColumn('uuid', { default: () => 'uuidv7()' })
+  id: string;
+
+  @Index('IDX_patient_person_id')
+  @Column('uuid')
+  person_id: string;
+
+  @Index('IDX_patient_medical_record_number')
+  @Column()
+  medical_record_number: string;
+
+  @Column({ default: true })
+  is_active: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Column('jsonb', { nullable: true })
+  created_by: AuthUser | null;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
