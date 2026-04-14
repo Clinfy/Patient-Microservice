fc2c51f74f2b2fa305478f7564159fc7d403b59f
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-04-13 22:22:57 -0300

    Integrate `@nestjs/config` and `TypeOrmModule` with environment validation and PostgreSQL configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ENTITIES } from 'src/entities';
+import { validate } from 'src/config/env-validation';
 
 @Module({
-  imports: [],
+  imports: [
+    //Config Module
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate,
+    }),
+
+    //TypeOrm Module
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_HOST'),
+        entities: [...ENTITIES],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
