c04d1de9d7b9a73f9ebba1b080f92e5a1805305a
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-04-13 21:39:06 -0300

    Add `RelationshipType` enum for relationship representation

---
+++ src/interfaces/enums/relationship.enum.ts
@@ -0,0 +1,9 @@
+export enum RelationshipType {
+  HOLDER = 'HOLDER',
+  SON = 'SON',
+  DAUGHTER = 'DAUGHTER',
+  MOTHER = 'MOTHER',
+  FATHER = 'FATHER',
+  SPOUSE = 'SPOUSE',
+  OTHER = 'OTHER',
+}
