1bf261909c0ac632d776136689fdef7fcb553ac7
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-04-15 12:59:37 -0300

    Add `trust proxy` and global validation pipe configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import { useContainer } from 'class-validator';
+import { BadRequestException, HttpStatus, ValidationPipe } from '@nestjs/common';
+import { findFirstErrorCode, findFirstMessage } from 'src/common/utils/find-errors-data.util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Trust Proxy
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.set('trust proxy', true);
+
+  //Cookie Parser
+  app.use(cookieParser());
+
+  //Logs
+  useContainer(app.select(AppModule), { fallbackOnErrors: true }); // <—
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      validationError: { target: false, value: true },
+      exceptionFactory: (errors) => {
+        const errorCode = findFirstErrorCode(errors) ?? 'VALIDATION_ERROR';
+        const message = findFirstMessage(errors);
+        return new BadRequestException({
+          statusCode: HttpStatus.BAD_REQUEST,
+          errorCode,
+          message,
+        });
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
