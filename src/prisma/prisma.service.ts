82541b5e7c1170b4dc64bd26c93b524485eabfef
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-04-16 14:40:19 -0300

    Add `PrismaModule` and `PrismaService` for PostgreSQL integration

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const adapter = new PrismaPg(process.env.DATABASE_HOST!);
+    super({ adapter, log: ['info', 'warn', 'error'] });
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      await this.$queryRaw`SELECT 1`;
+      console.log('Connected to PostgreSQL');
+    } catch (error) {
+      console.error('Error connecting to PostgreSQL:', error);
+      throw error;
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('Disconnected from PostgreSQL');
+  }
+}
