8e9aca1aa6dfb1bdd135590cf596cf1cfd039dbc
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-04-17 13:50:44 -0300

    Add `create` method to `CoverageProviderService` with user context integration

--- src/services/coverage-provider/coverage-provider.service.ts
+++ src/services/coverage-provider/coverage-provider.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { CoverageProviderRepository } from 'src/services/coverage-provider/coverage-provider.repository';
+import { CoverageProvider } from 'generated/prisma/client';
+import { CreateCoverageProviderDto } from 'src/interfaces/dto/coverage-provider.dto';
+import { RequestContextService } from 'src/common/context/request-context.service';
+import { toPrismaJsonUser } from 'src/common/utils/auth-user-mapper.util';
 
 @Injectable()
-export class CoverageProviderService {}
+export class CoverageProviderService {
+  constructor(
+    private readonly coverageProviderRepository: CoverageProviderRepository,
+    private readonly contextService: RequestContextService,
+  ) {}
+
+  async create(dto: CreateCoverageProviderDto): Promise<CoverageProvider> {
+    return this.coverageProviderRepository.save({
+      ...dto,
+      created_by: toPrismaJsonUser(this.contextService.getCurrentUser()),
+    });
+  }
+}
