e08ee5c04cd4054c39eb9bbeb78427bfa6f21b33
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-04-18 22:45:09 -0300

    Refactor `OutboxSubscriberService` to replace `action` with derived value based on `pattern` and enhance payload generation

--- src/cron/outbox.subscriber.service.ts
+++ src/cron/outbox.subscriber.service.ts
@@ -4,8 +4,7 @@ import { JsonObject, JsonValue } from '@prisma/client/runtime/client';
 
 export interface OutboxEvent {
   tx: Prisma.TransactionClient;
-  pattern: string;
-  action: string;
+  pattern: 'entity_created' | 'entity_updated' | 'entity_deleted';
   entity: string;
   entity_id: string;
   done_by?: JsonValue;
@@ -27,10 +26,23 @@ export class OutboxSubscriberService {
 
   private generatePayload(event: OutboxEvent): JsonObject {
     return {
-      action: event.action,
+      action: this.generateAction(event),
       entity: event.entity,
       entity_id: event.entity_id,
       done_by: event.done_by,
     };
+  }
+
+  private generateAction(event: OutboxEvent): string {
+    switch (event.pattern) {
+      case 'entity_created':
+        return event.entity.toUpperCase() + '_CREATED';
+      case 'entity_updated':
+        return event.entity.toUpperCase() + '_UPDATED';
+      case 'entity_deleted':
+        return event.entity.toUpperCase() + '_DELETED';
+      default:
+        return 'unknown';
+    }
   }
 }
