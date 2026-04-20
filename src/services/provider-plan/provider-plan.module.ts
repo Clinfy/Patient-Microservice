7ec80ccbab2d699ca1c35bfbb8a7f4651635693c
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-04-19 21:11:11 -0300

    Import `CoverageProviderModule` into `ProviderPlanModule` for coverage integration

--- src/services/provider-plan/provider-plan.module.ts
+++ src/services/provider-plan/provider-plan.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProviderPlanService } from './provider-plan.service';
 import { ProviderPlanController } from './provider-plan.controller';
 import { ProviderPlanRepository } from 'src/services/provider-plan/provider-plan.repository';
+import { CoverageProviderModule } from 'src/services/coverage-provider/coverage-provider.module';
 
 @Module({
+  imports: [CoverageProviderModule],
   providers: [ProviderPlanService, ProviderPlanRepository],
   controllers: [ProviderPlanController],
 })
