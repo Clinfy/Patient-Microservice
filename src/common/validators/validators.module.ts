4234f171907d72f976256229a17dc3dbfc43b14d
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-04-19 21:22:22 -0300

    Add `IsUniqueProviderPlanName` validator to `ValidatorsModule` for uniqueness enforcement

--- src/common/validators/validators.module.ts
+++ src/common/validators/validators.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { IsUniqueCoverageProviderNameConstraint } from 'src/common/validators/unique-coverage_provider-name.validator';
+import { IsUniqueProviderPlanNameConstraint } from 'src/common/validators/unique-provider_plan-name.validator';
 
 @Global()
 @Module({
-  providers: [IsUniqueCoverageProviderNameConstraint],
-  exports: [IsUniqueCoverageProviderNameConstraint],
+  providers: [IsUniqueCoverageProviderNameConstraint, IsUniqueProviderPlanNameConstraint],
+  exports: [IsUniqueCoverageProviderNameConstraint, IsUniqueProviderPlanNameConstraint],
 })
 export class ValidatorsModule {}
