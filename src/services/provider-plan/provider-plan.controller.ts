ecb6f464a777291645c7d0087ef8e95450b1ecda
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-04-19 21:45:00 -0300

    Add `createProviderPlan` endpoint to `ProviderPlanController`

--- src/services/provider-plan/provider-plan.controller.ts
+++ src/services/provider-plan/provider-plan.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ProviderPlan } from 'generated/prisma/client';
+import { ProviderPlanService } from 'src/services/provider-plan/provider-plan.service';
+import { CreateProviderPlanDto } from 'src/interfaces/dto/provider-plan.dto';
 
 @Controller('provider-plan')
-export class ProviderPlanController {}
+export class ProviderPlanController {
+  constructor(private readonly providerPlanService: ProviderPlanService) {}
+
+  @Post('new')
+  createProviderPlan(@Body() providerPlanDto: CreateProviderPlanDto): Promise<ProviderPlan> {
+    return this.providerPlanService.create(providerPlanDto);
+  }
+}
