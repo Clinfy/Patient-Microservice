796c90ee3bf2ed1bd832ba77b846515bb16ae02a
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-05-01 19:41:15 -0300

    Inject `PatientRepository` into `PatientModule` for dependency management

--- src/services/patient/patient.module.ts
+++ src/services/patient/patient.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PatientService } from './patient.service';
 import { PatientController } from './patient.controller';
+import { PatientRepository } from 'src/services/patient/patient.repository';
 
 @Module({
-  providers: [PatientService],
+  providers: [PatientService, PatientRepository],
   controllers: [PatientController],
 })
 export class PatientModule {}
