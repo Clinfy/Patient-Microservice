cb8f6f65dfed2dac6861e4e4378bd513520788e2
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-05-01 19:41:28 -0300

    Add `create` method to `PatientService` for handling patient creation with error handling

--- src/services/patient/patient.service.ts
+++ src/services/patient/patient.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { PatientRepository } from 'src/services/patient/patient.repository';
+import { Patient } from 'generated/prisma/client';
+import { RequestContextService } from 'src/common/context/request-context.service';
+import { CreatePatientDto } from 'src/interfaces/dto/patient.dto';
+import { toPrismaJsonUser } from 'src/common/mappers/auth-user.mapper';
+import { PatientErrorCodes, PatientException } from 'src/services/patient/patient.exception';
+import { getErrorStatus, getSafeError } from 'src/common/utils/get-safe-error.util';
 
 @Injectable()
-export class PatientService {}
+export class PatientService {
+  constructor(
+    private readonly patientRepository: PatientRepository,
+    private readonly contextService: RequestContextService,
+  ) {}
+
+  async create(dto: CreatePatientDto): Promise<Patient> {
+    try {
+      return this.patientRepository.save({
+        ...dto,
+        created_by: toPrismaJsonUser(this.contextService.getCurrentUser()),
+      });
+    } catch (error) {
+      throw new PatientException(
+        'Patient creation failed',
+        PatientErrorCodes.PATIENT_CREATION_FAILED,
+        getErrorStatus(error),
+        getSafeError(error),
+      );
+    }
+  }
+}
